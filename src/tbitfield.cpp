0c257182a9e35ed86fe0fdb30aee2a5afd93da7f
AveZorgen <KulikovArt2015@gmail.com>
2022-09-24 00:34:10 +0300

    test_tbitfield passed

--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -4,60 +4,90 @@
 //   Переработано для Microsoft Visual Studio 2008 Сысоевым А.В. (19.04.2015)
 //
 // Битовое поле
-
 #include "tbitfield.h"
 
+
 // Fake variables used as placeholders in tests
-static const int FAKE_INT = -1; //-1 status
+static const int FAKE_INT = -1; 
 static TBitField FAKE_BITFIELD(1);
 
 TBitField::TBitField(int len)
 {
+    if (len < 0)
+        throw length_error("length is negative");
+    BitLen = len;
+    MemLen = (BitLen - 1) / sizeof(TELEM) + 1;
+    pMem = new TELEM[MemLen];
+    for (int i = 0; i < MemLen; i++)
+        pMem[i] = 0;
 }
 
 TBitField::TBitField(const TBitField &bf) // конструктор копирования
 {
+    BitLen = bf.BitLen;
+    MemLen = bf.MemLen;
+    pMem = new TELEM[MemLen];
+    for (int i = 0; i < MemLen; i++)
+        pMem[i] = bf.pMem[i];
 }
 
 TBitField::~TBitField()
 {
+    if (pMem) delete[] pMem;
 }
 
 int TBitField::GetMemIndex(const int n) const // индекс Мем для бита n
 {
-    return FAKE_INT;
+    //return n / (sizeof(TELEM) * 8); // n / 32
+    return n >> 5;
 }
 
 TELEM TBitField::GetMemMask(const int n) const // битовая маска для бита n
 {
-    return FAKE_INT;
+    //return 1 << (n % (sizeof(TELEM) * 8));
+    return 1 << (n & (1 << 5 - 1));
 }
 
 // доступ к битам битового поля
 
 int TBitField::GetLength(void) const // получить длину (к-во битов)
 {
-  return FAKE_INT;
+    return BitLen;
 }
 
 void TBitField::SetBit(const int n) // установить бит
 {
+    if (n >= BitLen || n < 0)
+        throw out_of_range("bit pos is out of range");
+    pMem[GetMemIndex(n)] |= GetMemMask(n);
 }
 
 void TBitField::ClrBit(const int n) // очистить бит
 {
+    if (n >= BitLen || n < 0)
+        throw out_of_range("bit pos is out of range");
+    pMem[GetMemIndex(n)] &= ~GetMemMask(n);
 }
 
 int TBitField::GetBit(const int n) const // получить значение бита
 {
-  return FAKE_INT;
+    if (n >= BitLen || n < 0)
+        throw out_of_range("bit pos is out of range");
+    return pMem[GetMemIndex(n)] & GetMemMask(n);
 }
 
 // битовые операции
 
 TBitField& TBitField::operator=(const TBitField &bf) // присваивание
 {
-    return FAKE_BITFIELD;
+    if (&bf == this) return *this;
+    if (pMem) delete[] pMem;
+    BitLen = bf.BitLen;
+    MemLen = bf.MemLen;
+    pMem = new TELEM[MemLen];
+    for (int i = 0; i < MemLen; i++)
+        pMem[i] = bf.pMem[i];
+    return *this;
 }
 
 int TBitField::operator==(const TBitField &bf) const // сравнение
